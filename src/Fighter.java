734f4d712462e62d0ce91cb717d92bf321f17e97
AlvinChen529 <52618038+AlvinChen529@users.noreply.github.com>
2020-07-28 03:48:12 +1000

    Implement Tower placing and boarder checking for tower

---
+++ src/Fighter.java
@@ -0,0 +1,9 @@
+public class Fighter extends Tower{
+
+    private static String fighterImagePath = "res\\images\\Fighter.gif";
+
+    public Fighter() {
+        super(fighterImagePath, 1000);
+    }
+
+}
