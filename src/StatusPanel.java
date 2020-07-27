2fa9feadb7c9eeb7916c08a3cb1b18149f4a5dbb
AlvinChen529 <52618038+AlvinChen529@users.noreply.github.com>
2020-07-27 16:23:30 +1000

    implement fastforward click button

--- src/StatusPanel.java
+++ src/StatusPanel.java
@@ -18,10 +18,10 @@ public class StatusPanel {
     private final static Image pauseSign = new Image("res\\images\\pause.png");
     private final static Point PS_Location = new Point(940, 28);
 
-    private final static Image fastforwardSign = new Image("res\\images\\fastforward.png");
-    private final static Image fastbackwardSign = new Image("res\\images\\fastbackward.png");
-    private final static Point FFS_Location = new Point(150, 720);
-    private final static Point FBS_Location = new Point(110, 720);
+    private final static Image fastforwardSign = new Image("res\\images\\ff.png");
+    private final static Image fastbackwardSign = new Image("res\\images\\fb.png");
+    private final static Point FBS_Location = new Point(150, 725);
+    private final static Point FFS_Location = new Point(322, 725);
 
     private final static Rectangle PS_Rect = new Rectangle(PS_Location, 64,64);
     private final static Rectangle FFS_Rect = new Rectangle(FFS_Location, 44,44);
@@ -31,37 +31,42 @@ public class StatusPanel {
     private int pauseTimeScale = -1;
 
     public void drawPanel(Input input, int timeScale, double delayRemain, Game game){
-        //fastbackwardSign.drawFromTopLeft(FFS_Location.x, FFS_Location.y);
-
-        if (pauseTimeScale==-1) {
-
-            pauseSign.drawFromTopLeft(PS_Location.x, PS_Location.y);
-
-            if (input.wasPressed(Keys.K) && game.getTimeScale() - 1 >= 0) {
-                game.setTimeScale(game.getTimeScale() - 1);
-            }
-            if (input.wasPressed(Keys.L)) {
-                game.setTimeScale(game.getTimeScale() + 1);
-            }
-        }
+        fastbackwardSign.drawFromTopLeft(FBS_Location.x, FBS_Location.y);
+        fastforwardSign.drawFromTopLeft(FFS_Location.x, FFS_Location.y);
         if (input.wasPressed(Keys.N)) {
             game.setTimeScale(1);  // Normal speed
             pauseTimeScale = -1;
         }
+        if (pauseTimeScale==-1) {
+            if(timeScale!=0) {
+                pauseSign.drawFromTopLeft(PS_Location.x, PS_Location.y);
+            }
+            if (    (input.wasPressed(Keys.K)||
+                    (input.wasPressed(MouseButtons.LEFT) && FBS_Rect.intersects(input.getMousePosition())))
+                    && game.getTimeScale() - 1 >= 0) {
+                game.setTimeScale(game.getTimeScale() - 1);
+            }
+            if (input.wasPressed(Keys.L)||
+                    (input.wasPressed(MouseButtons.LEFT) && FFS_Rect.intersects(input.getMousePosition()) )) {
+
+                game.setTimeScale(game.getTimeScale() + 1);
+            }
+        }
         if (input.wasPressed(Keys.P) || (input.wasPressed(MouseButtons.LEFT) && PS_Rect.intersects(input.getMousePosition()))) {
-
-
-
-            if (pauseTimeScale==-1) {
-                pauseTimeScale = game.getTimeScale();
-                game.setTimeScale(0);
+            if (timeScale == 0 && pauseTimeScale==-1){
+                game.setTimeScale(1);
             }else{
-                game.setTimeScale(pauseTimeScale);
-                pauseTimeScale = -1;
+                if (pauseTimeScale==-1) {
+                    pauseTimeScale = game.getTimeScale();
+                    game.setTimeScale(0);
+                }else{
+                    game.setTimeScale(pauseTimeScale);
+                    pauseTimeScale = -1;
+                }
             }
         }
         if (timeScale>1){
-            panelFont.drawString("timescale >>" + timeScale + "x", timeScaleLocation.x, timeScaleLocation.y, green);
+            panelFont.drawString("timescale>>" + timeScale + "x", timeScaleLocation.x, timeScaleLocation.y, green);
         }else if (timeScale==1){
             panelFont.drawString("timescale  :" + timeScale + "x", timeScaleLocation.x, timeScaleLocation.y, black);
         }else{
